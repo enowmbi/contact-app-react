7384132c7847a4acaf57f847948a95b729213134
Enow Mbi <benowmbi@yahoo.com>
2024-02-11 17:18:46 +0100

    Add useEffect to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Header from './components/Header'
 import NewContactForm from './components/NewContactForm'
 import ContactList from './components/ContactList'
@@ -26,6 +26,18 @@ function App() {
         setContacts(filteredContacts)
         localStorage.setItem('contact-list', JSON.stringify(filteredContacts))
     }
+
+    const fetchUsers = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        setContacts(data)
+    }
+
+    useEffect(()=>{
+      fetchUsers()
+    },[])
+
+    
 
 
   return (

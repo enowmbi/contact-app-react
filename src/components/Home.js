e714f3b7475cc3ab0a78ff52e19b7ae62a85b8c6
Enow Mbi <benowmbi@yahoo.com>
2024-02-13 11:06:09 +0100

    Add link to Home page that navigates to new contact form

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom'
 import SearchContacts from  "../components/SearchContacts"
 import ContactTable from  "../components/ContactTable"
 
 const Home  = ({ searchText, setSearchText, contacts, handleEditContact, handleDeleteContact }) => {
     return(
         <section>
+            <section id="list-lable-and-link">
+                <h3>Contact List</h3>
+                <Link id="add-contact-link" to="/contacts/new">Add Contact</Link>
+            </section>
             <SearchContacts
                 searchText={searchText}
                 setSearchText={setSearchText}

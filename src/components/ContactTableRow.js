bec3bb88aa1584c457465ad88549a34c7ecbdc18
Enow Mbi <benowmbi@yahoo.com>
2024-02-13 11:14:30 +0100

    Change edit button to a link

--- src/components/ContactTableRow.js
+++ src/components/ContactTableRow.js
@@ -1,11 +1,12 @@
 import { FaTrashAlt, FaPencilAlt } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const ContactTableRow = ({contact, handleEditContact, handleDeleteContact})=>{
     return(
         <tr key={contact.id}>
             <td>{contact.name}</td>
             <td>{contact.email}</td>
-            <td>{<FaPencilAlt role="button" tabIndex="0" onClick={() => handleEditContact(contact.id)} />}</td>
+            <td><Link to={`/contact/${contact.id}/edit`}>{<FaPencilAlt role="button" tabIndex="0" />}</Link></td>
             <td>{<FaTrashAlt role="button" tabIndex="1" onClick={() => handleDeleteContact(contact.id)} />}</td>
         </tr>
     )
